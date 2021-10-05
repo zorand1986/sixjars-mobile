423a599bcbe2b184fbb3f20c64683d3f048ea68f
Zoran Damjanac <zorand1986@gmail.com>
2021-10-05 12:39:40 +0200

    configured basic eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
-  singleQuote: true,
+  singleQuote: trfalseue,
   trailingComma: 'all',
   arrowParens: 'avoid',
 };
