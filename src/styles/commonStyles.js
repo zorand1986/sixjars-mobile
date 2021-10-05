be5ef52a46ab4e87b3b555cd98fa7ef5c03accbc
Zoran Damjanac <zorand1986@gmail.com>
2021-10-05 12:39:02 +0200

    added common styles

---
+++ src/styles/commonStyles.js
@@ -0,0 +1,115 @@
+export const colors = {
+  primary: "#fb09bb",
+  secondary: "#03ce54",
+  tertiary: "#FCFE75",
+  placeholder: "#666666", // for placeholder
+  bgPrimary: "#282327",
+  bgSecondary: "#2F2F2F",
+  bgTertiary: "#454545",
+  black: "#000000",
+  low: "#6ab04c",
+  mid: "#f9ca24",
+  error: "#eb4d4b",
+  blue: "#c7ecee",
+  textColorPrimary: "#F7F7F7",
+  textColorSecondary: "#b3b3b3",
+  border: "#2c2c2c",
+};
+
+export const margin = {
+  small: 8,
+  medium: 12,
+  large: 16,
+};
+
+export const padding = {
+  small: 8,
+  medium: 12,
+  large: 16,
+};
+
+export const flexCenterBasis = {
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+export const borderRadius = {
+  primary: 8,
+};
+
+export const smallVerticalMargin = {
+  marginVertical: margin?.small,
+};
+
+export const mediumVerticalMargin = {
+  marginVertical: margin?.medium,
+};
+
+export const largeVerticalMargin = {
+  marginVertical: margin?.large,
+};
+
+export const smallVerticalPadding = {
+  paddingVertical: padding?.small,
+};
+
+export const mediumVerticalPadding = {
+  paddingVertical: padding?.medium,
+};
+
+export const largeVerticalPadding = {
+  paddingVertical: padding?.large,
+};
+
+export const smallHorizontalMargin = {
+  marginHorizontal: margin?.small,
+};
+
+export const mediumHorizontalMargin = {
+  marginHorizontal: margin?.medium,
+};
+
+export const largeHorizontalMargin = {
+  marginHorizontal: margin?.large,
+};
+
+export const smallHorizontalPadding = {
+  paddingHorizontal: padding?.small,
+};
+
+export const mediumHorizontalPadding = {
+  paddingHorizontal: padding?.medium,
+};
+
+export const largeHorizontalPadding = {
+  paddingHorizontal: padding?.large,
+};
+
+export const primaryBackground = {
+  backgroundColor: colors?.primary,
+};
+
+export const secondaryBackground = {
+  backgroundColor: colors?.secondary,
+};
+
+export const appBackgroundColorPrimary = {
+  backgroundColor: colors?.bgPrimary,
+};
+
+export const appBackgroundColorSecondary = {
+  backgroundColor: colors?.bgSecondary,
+};
+
+export const textColorPrimary = {
+  color: colors?.textColorPrimary,
+};
+
+export const textColorSecondary = {
+  color: colors?.textColorSecondary,
+};
+
+export const smallBorderRadius = {
+  borderRadius: 8,
+};
