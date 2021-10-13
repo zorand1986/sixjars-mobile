a93e485205a05d4b5f3d531d3af89d9e4f5c27f8
Zoran Damjanac <zorand1986@gmail.com>
2021-10-13 16:07:35 +0200

    added animation to progress bar

--- src/components/JarListItem.js
+++ src/components/JarListItem.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import * as Progress from "react-native-progress";
 import { alignments, colors, basicText } from "../styles/commonStyles";
 
-const JarListItem = ({Icon, code, progress, percentage}) => {
+const JarListItem = ({Icon, code, percentage, progress}) => {
+    const [indeterminate, setIndeterminate] = useState(true);
+
+    const animate = () => {
+        setTimeout(() => {
+          setIndeterminate(false);
+        }, 50);
+      };
+
+      useEffect(() => {
+        animate();
+      });
+
+
     return (
         <View style={[alignments.row, {height: 51}, alignments.alignCenter, alignments.justifyBetween]}>
             <View style={[{width: 30, height: 30, backgroundColor: colors.primary, borderRadius: 10}, alignments.alignCenter, alignments.justifyCenter]} >
@@ -13,9 +26,9 @@ const JarListItem = ({Icon, code, progress, percentage}) => {
             <Text style={basicText}>{code}</Text>
             </View>
             <View>
-                <Progress.Bar width={200} progress={progress} color={colors.primary} />
+                <Progress.Bar animationType="timing" width={200}  indeterminate={indeterminate} progress={progress} color={colors.primary} />
             </View>
-            <Text style={[basicText, {width: 30}]}>{percentage}</Text>
+            <Text style={[basicText, {width: 35}]}>{percentage}</Text>
         </View>
     );
 };
