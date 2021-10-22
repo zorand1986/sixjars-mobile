d17e46f39fab3c0798470d693cef72dba20a68f9
Zoran Damjanac <zorand1986@gmail.com>
2021-10-22 15:36:21 +0200

    Added help icon to JarListItem

--- src/components/JarListItem.jsx
+++ src/components/JarListItem.jsx
@@ -1,32 +1,64 @@
+/* eslint-disable import/no-unresolved */
 import React from "react";
-import { View, Text } from "react-native";
-
-import { alignments, colors, basicText } from "../styles/commonStyles";
+import { View, Text, TouchableOpacity } from "react-native";
+import {
+  alignments, colors, basicText, selectedJarItem, smallHorizontalPadding,
+} from "../styles/commonStyles";
 import ProgressBar from "./ProgressBar";
+import QuestionMark from "../../assets/QuestionMark";
 
 const JarListItem = ({
-  Icon, code, percentage, progress,
-}) => (
-  <View style={[
-    alignments.row,
-    { height: 51 },
-    alignments.alignCenter,
-    alignments.justifyBetween]}
-  >
-    <View style={[{
-      width: 30, height: 30, backgroundColor: colors.primary, borderRadius: 10,
-    }, alignments.alignCenter, alignments.justifyCenter]}
+  item,
+  selected,
+  touchable,
+  onPress,
+  setModalVisible,
+}) => {
+  const Wrapper = touchable ? TouchableOpacity : View;
+  const {
+    Icon, code, progress, percentage,
+  } = item;
+  return (
+    <Wrapper
+      onPress={onPress}
+      style={[
+        alignments.row,
+        { height: 51 },
+        alignments.alignCenter,
+        alignments.justifyBetween,
+        selected && selectedJarItem,
+        touchable && smallHorizontalPadding,
+      ]}
     >
-      <Icon width={17} height={17} fill="#000" />
-    </View>
-    <View style={{ width: 30 }}>
-      <Text style={basicText}>{code}</Text>
-    </View>
-    <View>
-      <ProgressBar width={progress * 200} />
-    </View>
-    <Text style={[basicText, { width: 35 }]}>{percentage}</Text>
-  </View>
-);
+      <View style={[{
+        width: 30,
+        height: 30,
+        backgroundColor: colors.primary,
+        borderRadius: 10,
+      },
+      alignments.alignCenter,
+      alignments.justifyCenter,
+      ]}
+      >
+        <Icon width={17} height={17} fill="#000" />
+      </View>
+      <View style={{ width: 30 }}>
+        <Text style={basicText}>{code}</Text>
+      </View>
+      <View>
+        <ProgressBar width={progress * 200} />
+      </View>
+      <Text style={[basicText, { width: 35 }]}>{percentage}</Text>
+      <TouchableOpacity onPressOut={() => setModalVisible(item?.id - 1)}>
+        <QuestionMark
+          width={17}
+          height={17}
+          fill={colors.textColorSecondary}
+          style={{ padding: 10 }}
+        />
+      </TouchableOpacity>
+    </Wrapper>
+  );
+};
 
 export default JarListItem;
