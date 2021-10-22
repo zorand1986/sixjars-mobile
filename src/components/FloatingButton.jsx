3e237cdf1438a6b7c60f023026f1bd84ec0e7fa2
Zoran Damjanac <zorand1986@gmail.com>
2021-10-22 15:38:15 +0200

    added FloatinButton navigation prop

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -5,19 +5,21 @@ import Add from "../../assets/Add";
 
 import { colors } from "../styles/commonStyles";
 
-const FloatingButton = () => (
-  <TouchableOpacity style={{
-    position: "absolute",
-    right: 30,
-    bottom: 80,
-    backgroundColor: colors.secondary,
-    borderRadius: 24,
-    width: 50,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 30,
-  }}
+const FloatingButton = ({ onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      position: "absolute",
+      right: 30,
+      bottom: 80,
+      backgroundColor: colors.secondary,
+      borderRadius: 24,
+      width: 50,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      zIndex: 30,
+    }}
   >
     <Add width={20} height={20} fill="#fff" />
   </TouchableOpacity>
